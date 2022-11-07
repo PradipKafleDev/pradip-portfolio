44b8b4aeef553bf6b96ecfe3197b5034640d2cb9
PradipKafleDev <pradipkafle074@gmail.com>
2022-11-07 12:09:48 +1100

    Portfolio section added

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const sections = document.querySelectorAll(".section");
 const sectBtns = document.querySelector(".controlls");
 const sectBtn = document.querySelectorAll(".control");
 const allSections = document.querySelectorAll(".main-content");
+const homeBtn = document.getElementById("home");
+const projectsBtn = document.getElementById("home");
+const contactBtn = document.getElementById("home");
+const navList = document.querySelector(".list");
+const listItem = document.querySelectorAll(".list-item");
+console.log(listItem);
 
 //selecting parent btns --event deligation
 sectBtns.addEventListener("click", function (e) {
@@ -20,5 +26,17 @@ sectBtns.addEventListener("click", function (e) {
   // console.log(currentBtn.dataset.id);
   document
     .querySelector(`.sec-${currentBtn.dataset.id}`)
+    .classList.add("active");
+});
+
+navList.addEventListener("click", e => {
+  const currentPage = e.target.closest(".list-item");
+  console.log(currentPage.dataset.id);
+
+  if (!currentPage) return;
+  sections.forEach(sec => sec.classList.remove("active"));
+
+  document
+    .querySelector(`.sec-${currentPage.dataset.id}`)
     .classList.add("active");
 });
